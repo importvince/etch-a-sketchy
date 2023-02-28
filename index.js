b1b9d489d2206f8cdec91f0f90a906acce5b28f8
Vinny 2 Thumbs <97123672+importvince@users.noreply.github.com>
2023-02-27 20:38:13 -0600

    Write psuedo code for JS

---
+++ index.js
@@ -0,0 +1,15 @@
+// 1. create a board of tiles based on the value of the slider
+// - initiate a variable for slider
+// - capture value of slider
+// - write a function that uses the value of the slider to create that amount of divs
+// - these divs should be styled to appear as squares with borders
+// 2. create event listeners on every created tile to listen for hovers
+// - create an array of the board
+// - write a forEach function that adds the same event listener to every div
+// parameters: should stay colored. passing over again shouldn't change it back. 
+// - should default to boring, but also an event listener on the boring button and can switch to random colors when pretty button is pressed. so there should be an event listener on the pretty button that runs the same forEach fucntion as above but includes a random color generator
+// - most styling will have the be done with javascript. includes color on hover and sizing of grid divs. and will have to maybe add grid styling to the #tile-container too...
+// 3. Add a reset button to clear the board
+// 4. Make it fun and different
+
+
